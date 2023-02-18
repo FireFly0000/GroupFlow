e6489f0818ca3a95b9785e503ea60ec005c8dade
FireFly0000 <wktfirefly283@gmail.com>
2023-02-18 14:20:53 -0800

    login/register authentication & home page set up

---
+++ backend/workflow/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from . import views
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('test/', views.testEndPoint, name='test'),
+    path('', views.getRoutes),
+    path('', include(router.urls))
+]
